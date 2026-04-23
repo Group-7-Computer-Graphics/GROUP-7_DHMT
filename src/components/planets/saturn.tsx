0d1f4d7fb421c8cfc5caa2c39e625c8d729743ef
24023079-max <24023079@vnu.edu.vn>
2026-04-23 16:28:11 +0700

    saturn.tsx
    sao thổ

--- src/components/planets/saturn.tsx
+++ src/components/planets/saturn.tsx
@@ -2,6 +2,7 @@ import Planet from "./Planet";
 import { Text } from "@react-three/drei";
 import { Dispatch, SetStateAction } from "react";
 import Button from "../Button";
+import { useTexture } from "@react-three/drei";
 
 interface PlanetProps {
     setControlsEnabled: Dispatch<SetStateAction<boolean>>;
@@ -10,7 +11,7 @@ interface PlanetProps {
 }
 const Saturn: React.FC<PlanetProps> = ({ setControlsEnabled, isActive, onClick }) => {
     const pos: [number, number, number] = [0, 0, 680]; 
-
+    const ringTexture = useTexture("/icons/2k_saturn.jpg");
     return (
         <group>
             <Planet
@@ -21,16 +22,20 @@ const Saturn: React.FC<PlanetProps> = ({ setControlsEnabled, isActive, onClick }
                 onClick={onClick || (() => { window.location.hash = "#saturn"; })}
             />
             {/* Vẽ vành đai Sao Thổ */}
-            <mesh position={pos} rotation={[Math.PI / 2.5, 0, 0]}>
-                <ringGeometry args={[12, 18, 64]} />
-                <meshStandardMaterial color="#c2a278" transparent opacity={0.5} side={2} />
+            <mesh position={[pos[0], pos[1], pos[2] + 2]} rotation={[Math.PI / 2.5, 0, 0]}>
+                <ringGeometry args={[18, 50, 64]} />
+                <meshBasicMaterial 
+                  map={ringTexture}
+                  transparent
+                  side={2}
+                />
             </mesh>
 
             {isActive && (
                 <group position={pos}>
                     <Text
                         color={"white"}
-                        position={[-16, 0, 0]}
+                        position={[-60, 0, 0]}
                         font="/fonts/futura_medium.ttf"
                         fontSize={1.2}
                         maxWidth={10}
@@ -39,7 +44,7 @@ const Saturn: React.FC<PlanetProps> = ({ setControlsEnabled, isActive, onClick }
                         Adorned with a dazzling system of icy rings, Saturn is unique in our solar system.
                     </Text>
                     <Button
-                        position={[16, 0, 0]}
+                        position={[60, 0, 0]}
                         text="Ring Research     ➜"
                         setControlsEnabled={setControlsEnabled}
                         buttonSize={[8, 1.5]}
