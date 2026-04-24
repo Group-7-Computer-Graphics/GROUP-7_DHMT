ce75748672390e7a36247da09e0a709ea609c029
pvan29493-max <pvan29493@gmail.com>
2026-04-24 18:22:31 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 // @ts-ignore
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
-import { HUDControls } from "../src/components/HUDControls"; 
+import { HUDControls } from "../src/components/HUDControl"; 
 import { EffectComposer, Bloom, ToneMapping } from "@react-three/postprocessing";
 
 // Import các hành tinh 
